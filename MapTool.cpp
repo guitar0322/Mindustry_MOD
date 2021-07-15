73af3a18e02a46cb5fd89a66813d34544ab69630
PKC135 <gnawk135@gmail.com>
2021-07-15 20:52:59 +0900

    캠페인 맵 구현

--- MapTool.cpp
+++ MapTool.cpp
@@ -42,7 +42,22 @@ void MapTool::Init()
 
 void MapTool::Update()
 {
-
+	if (KEYMANAGER->isStayKeyDown('W'))
+	{
+		MainCam->transform->MoveY(-15.f);
+	}
+	if (KEYMANAGER->isStayKeyDown('A'))
+	{
+		MainCam->transform->MoveX(-15.f);
+	}
+	if (KEYMANAGER->isStayKeyDown('S'))
+	{
+		MainCam->transform->MoveY(15.f);
+	}
+	if (KEYMANAGER->isStayKeyDown('D'))
+	{
+		MainCam->transform->MoveX(15.f);
+	}
 	/* =====UI 구현=====*/
 
 	//타일셋
